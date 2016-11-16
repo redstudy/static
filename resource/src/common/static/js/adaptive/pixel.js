503814c9fdfb87157ad7f982a125d66364932eb6
wanghongxin492 <wanghongxin492@emao.com>
2016-11-16 19:10:02 +0800

    bootstrap3栅格化

--- resource/src/common/static/js/adaptive/pixel.js
+++ resource/src/common/static/js/adaptive/pixel.js
@@ -1,10 +1,36 @@
 (function() {
+    __inline('./device.js');
+
     var dpr, rem, scale, width;
     var html = document.documentElement;
     var base = html.getAttribute('data-base') || 750;
     var meta = document.querySelector('meta[name="viewport"]');
     dpr = window.devicePixelRatio || 1;
     scale = 1 / dpr;
+
+    EVENTTYPE = {};
+    var flag=true;
+    if (device.mobile()) {
+        html.setAttribute('class', (html.getAttribute('class') || '' + ' mobile').trim());
+        EVENTTYPE.TAP = 'tap';
+    }
+    if (device.desktop()) {
+        flag=false;
+        if(html.getAttribute('data-from')=="mobile"){
+            html.setAttribute('class', (html.getAttribute('class') || '' + ' desktop').trim());
+            html.style.fontSize = '100px';
+        }else{
+            html.setAttribute('class', (html.getAttribute('class') || '' + ' desktop').trim());
+            // html.style.fontSize = '16px';
+        }
+        EVENTTYPE.TAP = 'click';
+    }
+    if (device.tablet()) {
+        html.setAttribute('class', (html.getAttribute('class') || '' + ' tablet').trim());
+        EVENTTYPE.TAP = 'tap';
+    }
+
+
 
     // 设置viewport，进行缩放，达到高清效果
     meta.setAttribute('content', 'width=device-width,initial-scale=' + scale + ',maximum-scale=' + scale + ', minimum-scale=' + scale + ',user-scalable=no');
@@ -16,10 +42,9 @@
 
     // 设置data-dpr以及font-size属性，留作的css hack之用
     html.setAttribute('data-dpr', dpr);
-    html.setAttribute('style', "font-size:" + rem + 'px');
+    flag&&html.setAttribute('style', "font-size:" + rem + 'px');
 
 
     window.dpr = dpr;
     window.rem = rem;
-
 })();
--- resource/src/page/startup/component/body/body.tpl
+++
@@ -1,5 +0,0 @@
-<!--
-    @require "./body.js"
-    @require "./body.scss"
--->
-<div class="body">body</div>
